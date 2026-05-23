597b3151423b891678ca73f41d28cfc7fd1c732c
André Gonzaga <andregonzaga95@gmail.com>
2026-05-23 13:20:12 -0300

    feat: Add Update Products Services

---
+++ src/modules/products/services/UpdateProductServices.ts
@@ -0,0 +1,39 @@
+import AppError from "@shared/errors/AppError";
+import { Product } from "../database/entities/Product";
+import { productsRepository } from "../database/repositories/ProductsRepositories";
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const product = await productsRepository.findById(id);
+
+    if (!product) {
+      throw new AppError("Product not found.", 404);
+    }
+
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists && name !== product.name) {
+      throw new AppError("There is already one product with this name");
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
