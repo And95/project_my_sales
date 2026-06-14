515771bebc4f97d1c4f8e7bfe620cc658143d0e4
André Gonzaga <andregonzaga95@gmail.com>
2026-06-14 14:58:15 -0300

    feat: Add Class-transformer for password and localhost/files/avatar

--- src/modules/users/controllers/UsersControllers.ts
+++ src/modules/users/controllers/UsersControllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import ListUserService from "@modules/users/services/ListUserService";
 import CreateUserService from "@modules/users/services/CreateUserService";
+import { instanceToInstance } from "class-transformer";
 
 interface CreateUserBody {
   name: string;
@@ -11,10 +12,8 @@ interface CreateUserBody {
 export default class UsersController {
   index = async (request: Request, response: Response): Promise<Response> => {
     const listUsers = new ListUserService();
-
     const users = await listUsers.execute();
-
-    return response.json(users);
+    return response.json(instanceToInstance(users));
   };
 
   create = async (
@@ -22,15 +21,13 @@ export default class UsersController {
     response: Response,
   ): Promise<Response> => {
     const { name, email, password } = request.body;
-
     const createUser = new CreateUserService();
-
     const user = await createUser.execute({
       name,
       email,
       password,
     });
 
-    return response.json(user);
+    return response.json(instanceToInstance(user));
   };
 }
